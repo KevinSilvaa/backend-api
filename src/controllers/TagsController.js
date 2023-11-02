ea6a2e6c9733845a886070d60b235560ff80e1c7
Kevin <kevinsilvarj10@gmail.com>
2023-11-02 15:05:28 -0300

    API quase finalizada

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,16 @@
+const knex = require('../database/knex');
+
+class TagsController {
+    
+    // *Listar todas as tags de um determinado usuário
+    async index(request, response) {
+        const { user_id } = request.params;
+
+        const tags = await knex("tags")
+        .where({ user_id });
+        
+        return response.json(tags);
+    }
+}
+
+module.exports = TagsController;
